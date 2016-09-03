d799329a9ae6f27820b1d9031742a925b310376d
irapfaya <arindomudiare@gmail.com>
2016-09-03 07:11:04 -0700

    rails frontend with helpers

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,18 @@
+class CustomersController < ApplicationController
+  PAGE_SIZE = 10
+    def index
+        if params[:keywords].present?
+           
+            @keywords = params[:keywords]
+            customer_search_term = CustomerSearchTerm.new(@keywords)
+            @customers = Customer.where(
+            customer_search_term.where_clause,
+            customer_search_term.where_args).
+            order(customer_search_term.order)
+        else
+        @customers = []
+        end
+    end
+
+
+end
